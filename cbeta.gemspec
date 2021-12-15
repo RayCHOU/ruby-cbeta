bc7e9ee95d6aabd2c00cc3fbb58e788805253e7b
Ray Chou <zhoubx@gmail.com>
2021-12-15 14:55:34 +0800

    new canon: TX

--- cbeta.gemspec
+++ cbeta.gemspec
@@ -1,8 +1,8 @@
 Gem::Specification.new do |s|
   s.name        = 'cbeta'
-  s.version     = '2.7.13'
+  s.version     = '2.7.14'
   s.license     = 'MIT'
-  s.date        = '2021-10-13'
+  s.date        = '2021-12-15'
   s.summary     = "CBETA Tools"
   s.description = "Ruby gem for use Chinese Buddhist Text resources made by CBETA (http://www.cbeta.org)."
   s.authors     = ["Ray Chou"]
--- test/test_cbeta.rb
+++ test/test_cbeta.rb
@@ -6,13 +6,4 @@ class CBETATest < Minitest::Test
     assert_equal CBETA.linehead_to_xml_file_path('GA009n0008_p0003a01'), 'GA/GA009/GA009n0008.xml'
     assert_equal CBETA.new.get_canon_symbol('TX'), '【太虛】'
   end
-  
-  # def test_gaiji_zhuyin
-  #   g = CBETA::Gaiji.new('/Users/ray/git-repos/cbeta_gaiji')
-
-  #   assert_equal ['ㄍㄢˇ', 'ㄍㄢ', 'ㄧㄤˊ', 'ㄇㄧˇ', 'ㄇㄧㄝ', 'ㄒㄧㄤˊ'],
-  #     g.zhuyin("CB00023")
-
-  #   assert_equal "[(王*巨)/木]", g['CB00006']['zzs']
-  # end
 end
